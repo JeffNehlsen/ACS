f313465dd2728b75f647bd42cc68a97a3c0ed7fe
Jeff Nehlsen <jeff.nehlsen@gmail.com>
2013-04-26 11:19:46 -0700

    Changes to bashing stuff

--- scripts/classes/Luminary.lua
+++ scripts/classes/Luminary.lua
@@ -120,6 +120,10 @@ class = {
     send("smite " .. selectedTarget)
     if not hasDefense("inspiration") then
       send("perform inspiration")
+    elseif tonumber(prompt.health) < 3500 then
+      send("angel drain")
+    elseif tonumber(angel.power) < 4200 then
+      send("angel power")
     end
   end,
   setup = function()
